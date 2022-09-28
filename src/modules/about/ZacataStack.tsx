aa1e32847b626e4760b21f920f5088f0ba5b2196
Luis <luisfroquez@gmail.com>
2022-09-28 00:18:31 -0300

    Stack: Se agrega infinite scroll.

--- src/modules/about/ZacataStack.tsx
+++ src/modules/about/ZacataStack.tsx
@@ -1,8 +1,9 @@
-import { Center, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Center, Flex, useColorModeValue, VStack } from '@chakra-ui/react'
+import styles from '../../styles/modules/home/stack.module.css'
 
 const ZacataStack = () => {
   const color = useColorModeValue('black', 'gray.100')
-  const grayBG = useColorModeValue('gray.50', 'gray.500')
+  const border = useColorModeValue('gray.300', 'gray.400')
 
   const items = [
     'HTML',
@@ -10,37 +11,82 @@ const ZacataStack = () => {
     'JAVASCRIPT',
     'TYPESCRIPT',
     'REACT',
+    'NEXT.JS',
+    'THREE.JS',
+    'TAILWIND CSS',
     'C#',
-    'XAMARIN'
+    'XAMARIN',
+    'FIGMA',
+    'ADOBE ILLUSTRATOR',
+    'ADOBE PHOTOSHOP',
+    'ADOBE DIMENSION'
   ]
   return (
-    <Flex
-      w="90%"
-      maxW="49.5em"
-      m={{ base: '3em 0', md: '0 0 3em 0' }}
-      color={color}
-      scrollSnapType="x mandatory"
-      gap="0.5em"
-      minH="4em"
-      pb="0.5em"
-      overflowX="auto"
-      overflowY="hidden"
-    >
-      {items.map(e => {
-        return (
-          <Center
-            key={e}
-            h="100%"
-            minW="12em"
-            bg={grayBG}
-            borderRadius={8}
-            scrollSnapAlign="start"
-          >
-            <p>{e}</p>
-          </Center>
-        )
-      })}
-    </Flex>
+    <VStack spacing={0} m={{ base: '3em 0', md: '0 0 3em 0' }}>
+      <Flex
+        w="100%"
+        maxW="49.5em"
+        color={color}
+        // scrollSnapType="x mandatory"
+        gap="0.5em"
+        minH="2em"
+        h="3em"
+        pb="0.5em"
+        className={styles.slider}
+      >
+        <div className={styles.slideTrack}>
+          {items.map((e, i) => {
+            return (
+              <Center
+                key={i}
+                h="100%"
+                minW="12em"
+                border="1px solid"
+                borderColor={border}
+                borderRadius={8}
+                scrollSnapAlign="start"
+                _hover={{ bg: border }}
+                transition="all 0.8s"
+              >
+                <p>{e}</p>
+              </Center>
+            )
+          })}
+        </div>
+      </Flex>
+      <Flex
+        w="100%"
+        maxW="49.5em"
+        // m={{ base: '3em 0', md: '0 0 3em 0' }}
+        color={color}
+        // scrollSnapType="x mandatory"
+        gap="0.5em"
+        minH="2em"
+        h="3em"
+        pb="0.5em"
+        className={styles.slider}
+      >
+        <div className={styles.slideTrackReverse}>
+          {items.map((e, i) => {
+            return (
+              <Center
+                key={i}
+                h="100%"
+                minW="12em"
+                border="1px solid"
+                borderColor={border}
+                borderRadius={8}
+                scrollSnapAlign="start"
+                _hover={{ bg: border }}
+                transition="all 0.8s"
+              >
+                <p>{e}</p>
+              </Center>
+            )
+          })}
+        </div>
+      </Flex>
+    </VStack>
   )
 }
 
